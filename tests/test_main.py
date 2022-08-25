f4cec438b90a8f68b29f237f1b830727df0feb1e
5j9 <5j9@users.noreply.github.com>
2022-08-25 12:56:13 +0430

    test: add offline tests using pytest

---
+++ tests/test_main.py
@@ -0,0 +1,147 @@
+import subprocess
+from unittest.mock import patch, call, Mock
+from sys import executable as python
+
+from pytest import raises
+
+from pip_review.__main__ import main
+
+
+class FakePopen:
+
+    def __init__(self, stdout=None, stderr=None, returncode=0):
+        self.stdout = stdout
+        self.stderr = stderr
+        self.returncode = returncode
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return
+
+    def communicate(self):
+        return self.stdout, self.stderr
+
+    def poll(self):
+        return self.returncode
+
+    def wait(self, timeout=None):
+        return self.returncode
+
+
+def outdated_call(forwarded=None):
+    args = [
+        python, '-m', 'pip', 'list', '--outdated', '--disable-pip-version-check', '--format=json'
+    ]
+    if forwarded:
+        args = args[:5] + forwarded + args[5:]
+    return call(args, stdout=subprocess.PIPE)
+
+
+def simulate(sys_argv, fake_popens):
+
+    def wrap(test_func):
+
+        def run_simulated():
+            logger = Mock()
+            with (
+                patch('sys.argv', sys_argv),
+                patch('pip_review.__main__.setup_logging', return_value=logger),
+                patch('subprocess.Popen', side_effect=fake_popens) as popen
+            ):
+                main()
+                test_func(popen, logger)
+
+        return run_simulated
+
+    return wrap
+
+
+UP_TO_DATE = FakePopen(b'[]\r\n')
+
+
+@simulate(
+    [''],
+    [UP_TO_DATE],
+)
+def test_everything_is_up_to_date(popen, logger):
+    assert popen.call_args_list == [outdated_call()]
+    assert logger.mock_calls == [call.info('Everything up-to-date')]
+
+
+@simulate(
+    [''],
+    [
+        FakePopen(
+            b'[{"name": "setuptools", "version": "65.1.1", "latest_version": "65.3.0", "latest_filetype": "wheel"}]\r\n'
+        ),
+    ],
+)
+def test_single_outdated_package(popen, logger):
+    assert popen.call_args_list == [outdated_call()]
+    assert logger.mock_calls == [call.info('setuptools==65.3.0 is available (you have 65.1.1)')]
+
+
+OUTDATED_SETUPTOOLS = FakePopen(
+    b'[{"name": "setuptools", "version": "65.1.1", "latest_version": "65.3.0", "latest_filetype": "wheel"}]\r\n'
+)
+
+
+@simulate(
+    ['', '--raw'],
+    [OUTDATED_SETUPTOOLS],
+)
+def test_raw_option(popen, logger):
+    assert popen.call_args_list == [outdated_call()]
+    assert logger.mock_calls == [call.info('setuptools==65.3.0')]
+
+
+@simulate(
+    ['', '--timeout', '30'],
+    [UP_TO_DATE],
+)
+def test_forwarding_unrecognized_args(popen, logger):
+    assert popen.call_args == outdated_call(['--timeout', '30'])
+    assert logger.mock_calls == [call.info('Everything up-to-date')]
+
+
+def test_forwarding_unrecognized_args_fails():
+    with raises(subprocess.CalledProcessError):
+        simulate(
+            ['', '--bananas'],
+            [FakePopen(b'[]\r\n', returncode=-1)],
+        )(lambda: None)()
+
+
+@simulate(
+    ['', '--auto'],
+    [UP_TO_DATE],
+)
+def test_auto_up_to_date(popen, logger):
+    assert popen.call_args_list == [outdated_call()]
+    assert logger.mock_calls == [call.info('Everything up-to-date')]
+
+
+@simulate(
+    ['', '--auto', '--force-reinstall'],
+    [OUTDATED_SETUPTOOLS, FakePopen()],
+)
+def test_forwarding_to_install_not_list(popen, logger):
+    assert popen.call_args_list[0] == outdated_call()
+    assert popen.call_args_list[1].args[0] == [
+        python, '-m', 'pip', 'install', '-U', '--force-reinstall', 'setuptools']
+    assert popen.call_count == 2
+    logger.assert_not_called()
+
+
+@simulate(
+    ['', '--auto', '--not-required'],
+    [OUTDATED_SETUPTOOLS, FakePopen()],
+)
+def test_forwarding_to_list_not_install(popen, logger):
+    assert popen.call_args_list[0] == outdated_call(['--not-required'])
+    assert popen.call_args.args[0] == [
+        python, '-m', 'pip', 'install', '-U', 'setuptools']
+    assert popen.call_count == 2
+    logger.assert_not_called()
